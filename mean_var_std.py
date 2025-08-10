b1a16f6857b0e250cc28a2b3a029dab929309f01
Kofi Asante <asantegyebikofi@gmail.com>
2025-08-10 11:32:00 +0000

    Complete FCC Python project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,19 @@
 import numpy as np
 
 def calculate(list):
-
+    arr = np.array(list)
+    if arr.size != 9:
+        raise ValueError("List must contain nine numbers.")
+    arr = arr.reshape((3,3))
+    
+    calculations = {
+            'mean': [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(arr).tolist()],
+            'variance': [np.var(arr, axis=0).tolist(), np.var(arr, axis=1).tolist(), np.var(arr).tolist()],
+            'standard deviation': [np.std(arr, axis=0).tolist(), np.std(arr, axis=1).tolist(), np.std(arr).tolist()],
+            'min': [ np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(arr).tolist()],
+            'max': [np.max(arr, axis=0).tolist(), np.max(arr, axis=1).tolist(), np.max(arr).tolist()],
+            'sum': [np.sum(arr, axis=0).tolist(), np.sum(arr, axis=1).tolist(), np.sum(arr).tolist()]
+    }
 
 
 
